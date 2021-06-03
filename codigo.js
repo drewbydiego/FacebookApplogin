534cdac73dfbf0900d8441cb8c5e11e73470675a
Diego Flores <diegoflo64411@gmail.com>
2021-06-03 10:55:09 -0600

    Agregando redireccion

---
+++ codigo.js
@@ -0,0 +1,7 @@
+const BTN_INICIO = document.querySelector('.btn');
+var ancho;
+var alto;
+BTN_INICIO.addEventListener('click',function(){
+    window.open("index.html", "Log", ancho = window.innerWidth,alto = window.innerHeight);
+    window.location.href = "index.html";
+});
