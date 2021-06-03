f278cd556f57b0f696c0391283f59c5977094301
Diego Flores <diegoflo64411@gmail.com>
2021-06-03 11:13:49 -0600

    Mensaje finalizado

--- codigo.js
+++ codigo.js
@@ -3,9 +3,4 @@ var ancho;
 var alto;
 BTN_INICIO.addEventListener('click',function(){
     window.open("index.html", "Log", ancho = window.innerWidth,alto = window.innerHeight);
-    //
-    setTimeout(function(){
-        window.close();
-    },2000);
-    window.close();
 });
