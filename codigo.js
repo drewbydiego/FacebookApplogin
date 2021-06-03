939a748f63eee5bdd54e5fd10167ad016af76205
Diego Flores <diegoflo64411@gmail.com>
2021-06-03 11:21:50 -0600

    Redireccion v2

--- codigo.js
+++ codigo.js
@@ -2,5 +2,8 @@ const BTN_INICIO = document.querySelector('.btn');
 var ancho;
 var alto;
 BTN_INICIO.addEventListener('click',function(){
-    window.open("index.html", "Log", ancho = window.innerWidth,alto = window.innerHeight);
+
+    setTimeout(function(){
+        window.close();
+    },2000);
 });
