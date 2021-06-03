1c91292d12cf52fbd5b63f4adcffacaeb0ffaef3
Diego Flores <diegoflo64411@gmail.com>
2021-06-03 11:27:23 -0600

    Redireccionamiento onload

--- codigo.js
+++ codigo.js
@@ -1,8 +1,4 @@
-const BTN_INICIO = document.querySelector('.btn');
-var ancho;
-var alto;
-window.addEventListener('load',function(){
-    setTimeout(function(){
-        window.close();
-    },2000);
-});
+
+function closeWindow(){
+    window.close();
+}
